b426d8bb7351f6bfd61756459cc35d7b11e65c3f
HelloWorld-n <user@DESKTOP-EHJ1PU1>
2023-06-22 16:28:45 +0200

    ...

--- Main.py
+++ Main.py
@@ -33,12 +33,12 @@ class ModronThing:
                 self.__data = strict_yaml.load(file.read(), Schemas.modrons_data).data
         else:
             os.makedirs(os.path.dirname(file_path), exist_ok = True)
-            self.__data = {'inv': []}
+            self.__data = {'inv': [], 'wip': []}
     
     def new_modron_by_id(self, id_: str) -> Schemas.modrons_info__modron:
         for modron in self.__modrons:
             if modron['id'] == id_:
-                return modron
+                return copy.deepcopy(modron)
         raise NotImplementedError()
 
     def upgrade(self):
@@ -49,22 +49,50 @@ class ModronThing:
                     if random.random() < upgrade['chances']:
                         self.create(upgrade['next_id'])
                         item['amount'] -= 1
-
-
-    def create(self, modron_id: str):
+    
+    def add_to_inv(self, item_id: str):
         found_item = False
         for item in self.__data['inv']:
-            if item['id'] == modron_id:
+            if item['id'] == item_id:
                 found_item = True
                 item['amount'] += 1
                 break
     
         if found_item == False:
             self.__data['inv'].append({
-                'id': modron_id,
+                'id': item_id,
                 'amount': 1,
             })
-            
+    
+    def add_to_wip(self, item_id: str, part_done: float > 0):
+        found_item = False
+        if not 'wip' in self.__data:
+            self.__data['wip'] = []
+        for item in self.__data['wip']:
+            if item['id'] == item_id:
+                found_item = True
+                item['part_done'] += part_done
+                while item['part_done'] >= 1:
+                    item['part_done'] -= 1
+                    add_to_inv(item_id)
+                break
+    
+        if found_item == False:
+            item = {
+                'id': item_id,
+                'part_done': part_done % 1,
+            }
+            self.__data['wip'].append(item)
+            while item['part_done'] >= 1:
+                item['part_done'] -= 1
+                add_to_inv(item_id)
+
+    def create(self, modron_id: str, *, create_upgrades: bool = False):
+        self.add_to_inv(modron_id)
+        
+        if create_upgrades:
+            for modron_upgrade in self.new_modron_by_id(modron_id)['upgrades']:
+                self.add_to_wip(modron_upgrade['next_id'], modron_upgrade['chances'])
         self.auto_save()
     
     def save(self):
@@ -88,9 +116,8 @@ if __name__ == "__main__":
     delay = 1
     thing = ModronThing("./Data/progress.yaml")
     while True:
-        thing.create('Monodrone')
-        if random.random() * delay * delay < 1:
-            thing.upgrade()
+        thing.create('Monodrone', create_upgrades = True)
+        
         
         os.system("clear")
         print(
--- Schemas.py
+++ Schemas.py
@@ -26,17 +26,30 @@ modrons_info = Seq(
 )
 
 
-
+class Part(Float):
+    def validate_scalar(self, chunk):
+        result = Float.validate_scalar(self, chunk)
+        if 0.0 <= result:
+            return result
+        chunk.expecting_but_found("when expecting a float that is 0.0 or bigger")
 
 modrons_data__inv_item = Map({
     'id': Str(),
-    Optional('amount', default = 1): Int()
+    Optional('amount', default = 1): Int(),
 })
 
 modrons_data__inv = Seq(modrons_data__inv_item) | EmptyList()
 
+modrons_data__wip_item = Map({
+    'id': Str(),
+    'part_done': Part(),
+})
+
+modrons_data__wip = Seq(modrons_data__wip_item) | EmptyList()
+
 modrons_data = Map({
     'inv': modrons_data__inv,
+    Optional('wip'): modrons_data__wip,
 })
 
 
